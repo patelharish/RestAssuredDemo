fab58e95098b5d743749a8603302a92e8ad75178
patelharish <patelharish304@gmail.com>
2024-07-17 18:21:04 +0530

    second commit

--- src/test/java/OAuth2/oAuthorization/AuthorizationOAuth.java
+++ src/test/java/OAuth2/oAuthorization/AuthorizationOAuth.java
@@ -1,5 +1,15 @@
 package OAuth2.oAuthorization;
 
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
 public class AuthorizationOAuth {
 
+	@Test
+	public void Oauth2Demo() {
+		RequestSpecification reqSpec = RestAssured.given();
+		
+	}
 }
